078d466473e9e155e73cfd772782c5562ad83285
prince yadav <princeyadav253737@gmail.com>
2026-04-12 13:55:58 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =14567
+let accountEmail = "prince@kumar.com"
+var accountPassword ="1234"
+accountcity ="jaipur"
+let accountstate ;
+
+//accountId = 246 // const hai change nhi kr skte 
+
+accountEmail="rounak@kumar.com"
+accountPassword="prince"
+accountcity ="bengluru"
+
+console.log(accountId)
+/* prefer not to use var because of issue in block scope and functional scope
+*/
+console.table([accountId,accountPassword,accountcity,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"  //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs,not browser 
+
+console.log(3+3)
+console.log("prince yadav")
+
+
+let name ="prince"
+let age =20
+let isloggin = false
+let state = null
+
+console.table([name,age,isloggin,state])
+
+// premitive data types name..
+
+// number =>2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null =>standardlone value
+// undefined => 
+// symbol => unique  
+
+// objet 
+
+console.log(typeof "prince")
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
+
+
+

bbc9dce8483ca98537072f99b466fcd5a8fe53b3
prince yadav <princeyadav253737@gmail.com>
2026-04-20 15:11:04 +0000

    cobersionoperation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,26 +1,65 @@
 // string to number
 
-//let score= "33"
-//console.log(score)
+/*let score= "33"
+console.log(score)
 
-//let valueInNumber = Number(score)
-//console.log(typeof valueInNumber)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber) */
 
 // number to string 
-//let prince = 22
-//console.log(typeof prince)
 
-//let saket = String(prince)
-//console.log(typeof saket)
+/*let prince = 22
+console.log(typeof prince)
+
+let saket = String(prince)
+console.log(typeof saket) */
 
 // "33" =>33
 // "33abc" =>NaN
 // true=> 1;  false=>0
 
-let isloggedin =1
+/*let isloggedin =1
 let booleanisloggedin = Boolean(isloggedin)
-console.log(booleanisloggedin)
+console.log(booleanisloggedin) */
 
 // 1=> true; 0=> false
 // "" => false 
 //"prince" => true
+
+// ************* operation *************
+
+/* let value = 3
+let negvalue = -value 
+console.log(negvalue) 
+
+console.log(2-2)
+console.log(2+2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**2) */
+
+/*let str1 = "prince"
+let str2=  " yadav"
+let str3= str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(1+"2"+"2")
+console.log("1"+"2") */
+
+/*console.log(+true)
+console.log(+"") */
+
+ /*let gamecounter = 100
+++gamecounter 
+console.log(gamecounter)
+
+let print = 100
+print++
+console.log(print) */
+
+

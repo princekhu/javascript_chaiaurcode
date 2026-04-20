c9d7d38567c6215e4c31569bb94840f5f41da2a9
prince yadav <princeyadav253737@gmail.com>
2026-04-20 08:06:43 +0000

    conversion of data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+// string to number
+
+//let score= "33"
+//console.log(score)
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+// number to string 
+//let prince = 22
+//console.log(typeof prince)
+
+//let saket = String(prince)
+//console.log(typeof saket)
+
+// "33" =>33
+// "33abc" =>NaN
+// true=> 1;  false=>0
+
+let isloggedin =1
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin)
+
+// 1=> true; 0=> false
+// "" => false 
+//"prince" => true
